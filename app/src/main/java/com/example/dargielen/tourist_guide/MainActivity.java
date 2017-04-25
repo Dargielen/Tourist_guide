0092b2edbcc5d7fbcf8ad31881dd7c51c493dc61
Dargielen <dargielen@gmail.com>
2017-04-25 21:10:02 +0200

    wersja przedwstępna 6

--- app/src/main/java/com/example/dargielen/tourist_guide/MainActivity.java
+++ app/src/main/java/com/example/dargielen/tourist_guide/MainActivity.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
